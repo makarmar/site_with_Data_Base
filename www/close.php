2a7fbd95c1855530b481c0fbe18223bc5d612da5
Maria <mcrowan@mail.ru>
2014-11-03 20:34:53 +0300

    Delete close.php

--- www/close.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();//открытие сессии 
-unset($_SESSION['login']);//закрытие сессии по логину 
-session_destroy();//удаление сессии 
-header("Location: http://db2.ru/vhod.php");//Перенаправление на эту страницу после нажатия кнопки ВЫЙТИ 
-?>
